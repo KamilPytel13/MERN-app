d05359d55392124ea0b0ad2b4a181c53928fe7cb
Kamil Pytel <k.s.pytel@gmail.com>
2022-06-02 12:44:59 +0100

    changed post item styles a bit

--- src/users/components/PostItem.js
+++ src/users/components/PostItem.js
@@ -1,14 +1,29 @@
 import Card from "../../shared/components/UIElements/Card";
-import './UserItem.css';
+import Button from "../../shared/components/FormElements/Button";
+//import './UserItem.css';
+import './PostItem.css';
 
 const PostItem = props => {
+    // return (
+    //     <li className='user-item'>
+    //         <Card className='user-item__content'>
+    //             <div className='user-item__info'>
+    //                 <h2>Title: {props.title}</h2>
+    //                 <h2>Description: {props.description}</h2>
+    //             </div>
+    //         </Card>
+    //     </li>
+    // );
     return (
         <li className='user-item'>
-            <Card className='user-item__content'>
-                <div className='user-item__info'>
-                    <h2>Title: {props.title}</h2>
+            {/* <Card className='user-item__content'> */}
+            <Card>
+                <Button>x</Button>
+                <div className="post-content">
+                    <h1 className="post-title">Title: {props.title}</h1>
                     <h2>Description: {props.description}</h2>
                 </div>
+                <Button>Like</Button>
             </Card>
         </li>
     );
